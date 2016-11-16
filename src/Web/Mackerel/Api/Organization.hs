6c3a9674449932f1d04a6c82960bf8fcb24da220
itchyny <itchyny@hatena.ne.jp>
2016-11-16 23:06:08 +0900

    add GET Organization API

---
+++ src/Web/Mackerel/Api/Organization.hs
@@ -0,0 +1,11 @@
+-- | Organization API.
+module Web.Mackerel.Api.Organization (getOrganization) where
+
+import Network.Http.Client
+
+import Web.Mackerel.Client
+import Web.Mackerel.Internal.Api
+import Web.Mackerel.Types.Organization
+
+getOrganization :: Client -> IO (Either ApiError Organization)
+getOrganization client = request client GET "/api/v0/org" emptyBody (createHandler id)

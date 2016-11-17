0da94b1124d00994314b3fc08808de06caa72c74
itchyny <itchyny@hatena.ne.jp>
2016-11-17 20:59:09 +0900

    add Alert APIs

--- src/Web/Mackerel/Api.hs
+++ src/Web/Mackerel/Api.hs
@@ -1,5 +1,6 @@
 module Web.Mackerel.Api (module Api) where
 
+import Web.Mackerel.Api.Alert as Api
 import Web.Mackerel.Api.Host as Api
 import Web.Mackerel.Api.Organization as Api
 import Web.Mackerel.Api.Role as Api
---
+++ src/Web/Mackerel/Api/Alert.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE TemplateHaskell #-}
+-- | Alert API.
+module Web.Mackerel.Api.Alert (listAlerts, closeAlert) where
+
+import Data.Aeson (encode)
+import Data.Aeson.TH (deriveJSON)
+import qualified Data.HashMap.Lazy as HM
+import Network.Http.Client
+import System.IO.Streams.ByteString (fromLazyByteString)
+
+import Web.Mackerel.Client
+import Web.Mackerel.Internal.Api
+import Web.Mackerel.Internal.TH
+import Web.Mackerel.Types.Alert
+
+data ListAlertsResponse = ListAlertsResponse { responseAlerts :: [Alert] }
+$(deriveJSON options ''ListAlertsResponse)
+
+listAlerts :: Client -> IO (Either ApiError [Alert])
+listAlerts client
+  = request client GET "/api/v0/alerts" emptyBody (createHandler responseAlerts)
+
+closeAlert :: Client -> AlertId -> String -> IO (Either ApiError Alert)
+closeAlert client (AlertId alertId') reason = do
+  body <- inputStreamBody <$> fromLazyByteString (encode (HM.fromList [("reason", reason)]))
+  request client POST ("/api/v0/alerts/" ++ alertId' ++ "/close") body (createHandler id)

06cfd027d64a0076e0c1b08409b655839b889a15
itchyny <itchyny@hatena.ne.jp>
2016-11-19 10:24:41 +0900

    update listHosts API: support query to filter by service, role, name and status

--- src/Web/Mackerel/Api/Host.hs
+++ src/Web/Mackerel/Api/Host.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings, TemplateHaskell #-}
 -- | Host API.
 module Web.Mackerel.Api.Host
   ( getHost
@@ -25,12 +25,27 @@ getHost :: Client -> HostId -> IO (Either ApiError Host)
 getHost client (HostId hostId')
   = request client GET ("/api/v0/hosts/" ++ hostId') [] emptyBody (createHandler responseHost)
 
+data ListHostsParams
+  = ListHostsParams {
+    listHostsParamsService :: Maybe String,
+    listHostsParamsRoles :: [String],
+    listHostsParamsName :: [String],
+    listHostsParamsStatus :: [HostStatus]
+  }
+
+instance Default ListHostsParams where
+  def = ListHostsParams Nothing [] [] [HostStatusWorking, HostStatusStandby, HostStatusMaintenance, HostStatusPoweroff]
+
 data ListHostsResponse = ListHostsResponse { responseHosts :: [Host] }
 $(deriveJSON options ''ListHostsResponse)
 
-listHosts :: Client -> IO (Either ApiError [Host])
-listHosts client
-  = request client GET "/api/v0/hosts" [] emptyBody (createHandler responseHosts)
+listHosts :: Client -> ListHostsParams -> IO (Either ApiError [Host])
+listHosts client params = do
+  let query = [ ("service", Just $ BS.pack s) | s <- maybeToList (listHostsParamsService params) ] ++
+              [ ("role", Just $ BS.pack r) | r <- listHostsParamsRoles params ] ++
+              [ ("name", Just $ BS.pack r) | r <- listHostsParamsName params ] ++
+              [ ("status", Just $ BS.pack $ show s) | s <- listHostsParamsStatus params ]
+  request client GET "/api/v0/hosts" query emptyBody (createHandler responseHosts)
 
 data ListMetricNamesResponse = ListMetricNamesResponse { responseNames :: [String] }
 $(deriveJSON options ''ListMetricNamesResponse)

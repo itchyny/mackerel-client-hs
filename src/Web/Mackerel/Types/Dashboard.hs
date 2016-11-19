a259f8751c94cf20b45108a576257254e5394a08
itchyny <itchyny@hatena.ne.jp>
2016-11-20 06:47:24 +0900

    add Web.Mackerel.Types.Dashboard

---
+++ src/Web/Mackerel/Types/Dashboard.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Web.Mackerel.Types.Dashboard where
+
+import Data.Aeson
+import qualified Data.Aeson as Aeson
+import Data.Aeson.TH (deriveJSON, fieldLabelModifier)
+import Data.Aeson.Types (typeMismatch)
+import Data.Char (toLower)
+import qualified Data.Text as Text
+
+import Web.Mackerel.Internal.TH
+
+data DashboardId = DashboardId String
+            deriving (Eq, Show)
+
+instance FromJSON DashboardId where
+  parseJSON (Aeson.String dashboardId') = return $ DashboardId $ Text.unpack dashboardId'
+  parseJSON o = typeMismatch "DashboardId" o
+
+instance ToJSON DashboardId where
+  toJSON (DashboardId dashboardId') = toJSON dashboardId'
+
+data DashboardCreate
+  = DashboardCreate {
+    dashboardCreateTitle :: String,
+    dashboardCreateBodyMarkdown :: String,
+    dashboardCreateUrlPath :: String
+  } deriving (Eq, Show)
+
+$(deriveJSON options { fieldLabelModifier = (\(c:cs) -> toLower c : cs) . drop 15 } ''DashboardCreate)
+
+data Dashboard
+  = Dashboard {
+    dashboardId :: DashboardId,
+    dashboardTitle :: String,
+    dashboardBodyMarkdown :: String,
+    dashboardUrlPath :: String,
+    dashboardCreatedAt :: Integer,
+    dashboardUpdatedAt :: Integer
+  } deriving (Eq, Show)
+
+$(deriveJSON options ''Dashboard)

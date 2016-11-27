986a06516c8a5bf1f2726ba87c9a424515468381
itchyny <itchyny@hatena.ne.jp>
2016-11-27 21:20:41 +0900

    add tests for Config

---
+++ test/ConfigSpec.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE OverloadedStrings, QuasiQuotes #-}
+module ConfigSpec where
+
+import Control.Monad (forM_)
+import Data.Default
+import Test.Hspec
+import Text.RawString.QQ
+
+import Web.Mackerel.Config
+import Web.Mackerel.Types.Host
+
+spec :: Spec
+spec = do
+
+  let config1 = def {
+        configApiKey = Just "<Mackerel-API-KEY>"
+      }
+
+  let toml1 = [r|
+        apikey = "<Mackerel-API-KEY>"
+      |]
+
+  let config2 = Config {
+        configApiBase = Just "https://mackerel.io",
+        configApiKey = Just "<Mackerel-API-KEY>",
+        configRoot = Just "/var/lib/mackerel-agent",
+        configPidfile = Just "/var/run/mackerel-agent.pid",
+        configRoles = Just ["service1:role1", "service2:role3"],
+        configVerbose = Just True,
+        configDiagnostic = Just False,
+        configDisplayname = Just "Example Host",
+        configHostStatus = Just HostStatusConfig {
+          hostStatusOnStart = Just HostStatusWorking,
+          hostStatusOnStop = Just HostStatusPoweroff
+        }
+      }
+
+  let toml2 = [r|
+        apibase = "https://mackerel.io"
+        apikey = "<Mackerel-API-KEY>"
+        root = "/var/lib/mackerel-agent"
+        pidfile = "/var/run/mackerel-agent.pid"
+
+        verbose = true
+        diagnostic = false
+
+        display_name = "Example Host"
+
+        roles = [ "service1:role1", "service2:role3" ]
+
+        include = "/etc/mackerel-agent/conf.d/*.conf"
+
+        [host_status]
+        on_start = "working"
+        on_stop  = "poweroff"
+      |]
+
+  describe "parseConfig" $
+    it "should parse a configuration toml" $
+      forM_ [(toml1, config1), (toml2, config2)] $ \(toml, config) ->
+        parseConfig toml `shouldBe` Right config

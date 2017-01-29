568843367d3dd0fea804d6c4f328fe16cc6354cf
itchyny <itchyny@hatena.ne.jp>
2017-01-29 19:20:34 +0900

    add Web.Mackerel.Types.GraphAnnotation

--- src/Web/Mackerel/Types.hs
+++ src/Web/Mackerel/Types.hs
@@ -2,6 +2,7 @@ module Web.Mackerel.Types (module Types) where
 
 import Web.Mackerel.Types.Alert as Types
 import Web.Mackerel.Types.Dashboard as Types
+import Web.Mackerel.Types.GraphAnnotation as Types
 import Web.Mackerel.Types.Host as Types
 import Web.Mackerel.Types.Monitor as Types
 import Web.Mackerel.Types.Organization as Types
---
+++ src/Web/Mackerel/Types/GraphAnnotation.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Web.Mackerel.Types.GraphAnnotation where
+
+import Data.Aeson
+import qualified Data.Aeson as Aeson
+import Data.Aeson.TH (deriveJSON, fieldLabelModifier)
+import Data.Aeson.Types (typeMismatch)
+import Data.Char (toLower)
+import Data.Default (Default(..))
+import qualified Data.Text as Text
+
+import Web.Mackerel.Internal.TH
+
+data GraphAnnotationId = GraphAnnotationId String
+                       deriving (Eq, Show)
+
+instance FromJSON GraphAnnotationId where
+  parseJSON (Aeson.String graphAnnotationId') = return $ GraphAnnotationId $ Text.unpack graphAnnotationId'
+  parseJSON o = typeMismatch "GraphAnnotationId" o
+
+instance ToJSON GraphAnnotationId where
+  toJSON (GraphAnnotationId graphAnnotationId') = toJSON graphAnnotationId'
+
+data GraphAnnotation
+  = GraphAnnotation {
+    graphAnnotationId :: Maybe GraphAnnotationId,
+    graphAnnotationTitle :: String,
+    graphAnnotationDescription :: String,
+    graphAnnotationFrom :: Integer,
+    graphAnnotationTo :: Integer,
+    graphAnnotationService :: String,
+    graphAnnotationRoles :: Maybe [String]
+  } deriving (Eq, Show)
+
+instance Default GraphAnnotation where
+  def = GraphAnnotation def def def def def def def
+
+$(deriveJSON options { fieldLabelModifier = (\(c:cs) -> toLower c : cs) . drop 15 } ''GraphAnnotation)

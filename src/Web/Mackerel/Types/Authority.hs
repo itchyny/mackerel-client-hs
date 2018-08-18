c26a2e94106d0586d2238ee0b4788dafc3edc18f
itchyny <itchyny@hatena.ne.jp>
2018-08-18 21:03:47 +0900

    implement Authority

---
+++ src/Web/Mackerel/Types/Authority.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Web.Mackerel.Types.Authority where
+
+import Data.Aeson
+import qualified Data.Aeson as Aeson
+import Data.Aeson.Types (typeMismatch)
+import qualified Data.Text as Text
+
+data Authority = AuthorityOwner
+               | AuthorityManager
+               | AuthorityCollaborator
+               | AuthorityViewer
+               deriving Eq
+
+instance Show Authority where
+  show AuthorityOwner = "owner"
+  show AuthorityManager = "manager"
+  show AuthorityCollaborator = "collaborator"
+  show AuthorityViewer = "viewer"
+
+instance Read Authority where
+  readsPrec _ xs = [ (hs, drop (length str) xs) | (hs, str) <- pairs', take (length str) xs == str ]
+    where pairs' = [
+            (AuthorityOwner, "owner"),
+            (AuthorityManager, "manager"),
+            (AuthorityCollaborator, "collaborator"),
+            (AuthorityViewer, "viewer")]
+
+instance FromJSON Authority where
+  parseJSON (Aeson.String txt)
+    | str == "owner" = return AuthorityOwner
+    | str == "manager" = return AuthorityManager
+    | str == "collaborator" = return AuthorityCollaborator
+    | str == "viewer" = return AuthorityViewer
+    where str = Text.unpack txt
+  parseJSON o = typeMismatch "Authority" o
+
+instance ToJSON Authority where
+  toJSON method = toJSON (show method)

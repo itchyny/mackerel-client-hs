7e412ca5814f378a702a1234711041c9e7a47113
itchyny <itchyny@hatena.ne.jp>
2018-08-19 00:43:04 +0900

    implement Service APIs

--- src/Web/Mackerel/Api/Service.hs
+++ src/Web/Mackerel/Api/Service.hs
@@ -2,6 +2,8 @@
 -- | Service API.
 module Web.Mackerel.Api.Service
   ( listServices
+  , createService
+  , deleteService
   , listServiceMetricNames
   ) where
 
@@ -21,6 +23,14 @@ $(deriveJSON options ''ListServicesResponse)
 listServices :: Client -> IO (Either ApiError [Service])
 listServices client
   = request client GET "/api/v0/services" [] emptyBody (createHandler responseServices)
+
+createService :: Client -> Service -> IO (Either ApiError Service)
+createService client service
+  = request client POST "/api/v0/services" [] (Just service) (createHandler id)
+
+deleteService :: Client -> String -> IO (Either ApiError Service)
+deleteService client name
+  = request client DELETE ("/api/v0/services/" <> BS.pack name) [] emptyBody (createHandler id)
 
 data ListMetricNamesResponse = ListMetricNamesResponse { responseNames :: [String] }
 $(deriveJSON options ''ListMetricNamesResponse)

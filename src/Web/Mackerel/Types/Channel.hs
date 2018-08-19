f2e8171d0671ba73e283e572c348171b4ff9b35b
itchyny <itchyny@hatena.ne.jp>
2018-08-19 16:43:33 +0900

    add Web.Mackerel.Types.Channel

---
+++ src/Web/Mackerel/Types/Channel.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Web.Mackerel.Types.Channel where
+
+import Data.Aeson
+import qualified Data.Aeson as Aeson
+import Data.Aeson.TH (deriveJSON)
+import Data.Aeson.Types (typeMismatch)
+import qualified Data.Text as Text
+
+import Web.Mackerel.Internal.TH
+
+data ChannelId = ChannelId String
+               deriving (Eq, Show)
+
+instance FromJSON ChannelId where
+  parseJSON (Aeson.String channelId) = return $ ChannelId $ Text.unpack channelId
+  parseJSON o = typeMismatch "ChannelId" o
+
+instance ToJSON ChannelId where
+  toJSON (ChannelId channelId) = toJSON channelId
+
+data Channel
+  = Channel {
+    channelId :: ChannelId,
+    channelName :: String,
+    channelType :: String
+  } deriving (Eq, Show)
+
+$(deriveJSON options ''Channel)

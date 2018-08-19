da45f9893eaefd2c00aabf09e8b3d5613588517f
itchyny <itchyny@hatena.ne.jp>
2018-08-19 20:59:42 +0900

    remove unused imports

--- src/Web/Mackerel/Types/Invitation.hs
+++ src/Web/Mackerel/Types/Invitation.hs
@@ -1,11 +1,7 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Web.Mackerel.Types.Invitation where
 
-import Data.Aeson
-import qualified Data.Aeson as Aeson
 import Data.Aeson.TH (deriveJSON)
-import Data.Aeson.Types (typeMismatch)
-import qualified Data.Text as Text
 
 import Web.Mackerel.Internal.TH
 import Web.Mackerel.Types.Authority

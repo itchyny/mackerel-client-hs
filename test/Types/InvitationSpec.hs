440e70ed67cc40d5896c60148ba622e20e5a9a9f
itchyny <itchyny@hatena.ne.jp>
2018-10-08 12:24:55 +0900

    update tests for Invitation types

--- test/Types/InvitationSpec.hs
+++ test/Types/InvitationSpec.hs
@@ -15,13 +15,15 @@ spec = do
 
   let invitation' = Invitation {
         invitationEmail = "foobar@example.com",
-        invitationAuthority = AuthorityManager
+        invitationAuthority = AuthorityManager,
+        invitationExpiresAt = 1492393387
       }
 
   let json = [aesonQQ|
     {
       "email": "foobar@example.com",
-      "authority": "manager"
+      "authority": "manager",
+      "expiresAt": 1492393387
     }
   |]
 
@@ -32,9 +34,35 @@ spec = do
     it "should reject an invalid json" $ do
       decode "{}" `shouldBe` (Nothing :: Maybe Invitation)
       let (Object hm) = json
-      forM_ ["email", "authority"] $ \key ->
+      forM_ ["email", "authority", "expiresAt"] $ \key ->
         decode (encode (Object (HM.delete key hm))) `shouldBe` (Nothing :: Maybe Invitation)
 
-  describe "invitation ToJSON" $
+  describe "Invitation ToJSON" $
     it "should encode into a json" $
       decode (encode invitation') `shouldBe` Just json
+
+  let invitationCreate' = InvitationCreate {
+        invitationCreateEmail = "foobar@example.com",
+        invitationCreateAuthority = AuthorityCollaborator
+      }
+
+  let jsonCreate = [aesonQQ|
+    {
+      "email": "foobar@example.com",
+      "authority": "collaborator"
+    }
+  |]
+
+  describe "InvitationCreate FromJSON" $ do
+    it "should parse a json" $
+      decode (encode jsonCreate) `shouldBe` Just invitationCreate'
+
+    it "should reject an invalid json" $ do
+      decode "{}" `shouldBe` (Nothing :: Maybe InvitationCreate)
+      let (Object hm) = jsonCreate
+      forM_ ["email", "authority"] $ \key ->
+        decode (encode (Object (HM.delete key hm))) `shouldBe` (Nothing :: Maybe InvitationCreate)
+
+  describe "InvitationCreate ToJSON" $
+    it "should encode into a json" $
+      decode (encode invitationCreate') `shouldBe` Just jsonCreate

2919478664a6408f0b214c61ff1807ee89a05d5e
itchyny <itchyny@hatena.ne.jp>
2018-10-08 12:20:00 +0900

    add InvitationCreate type and invitationExpiresAt field to Invitation

--- src/Web/Mackerel/Api/Invitation.hs
+++ src/Web/Mackerel/Api/Invitation.hs
@@ -10,7 +10,7 @@ import Web.Mackerel.Client
 import Web.Mackerel.Internal.Api
 import Web.Mackerel.Types.Invitation
 
-createInvitation :: Client -> Invitation -> IO (Either ApiError Invitation)
+createInvitation :: Client -> InvitationCreate -> IO (Either ApiError Invitation)
 createInvitation client invitation
   = request client POST "/api/v0/invitations" [] (Just invitation) (createHandler id)
 
--- src/Web/Mackerel/Types/Invitation.hs
+++ src/Web/Mackerel/Types/Invitation.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Web.Mackerel.Types.Invitation where
 
-import Data.Aeson.TH (deriveJSON)
+import Data.Aeson.TH (deriveJSON, fieldLabelModifier)
+import Data.Char (toLower)
 
 import Web.Mackerel.Internal.TH
 import Web.Mackerel.Types.Authority
@@ -9,7 +10,16 @@ import Web.Mackerel.Types.Authority
 data Invitation
   = Invitation {
     invitationEmail :: String,
-    invitationAuthority :: Authority
+    invitationAuthority :: Authority,
+    invitationExpiresAt :: Integer
   } deriving (Eq, Show)
 
 $(deriveJSON options ''Invitation)
+
+data InvitationCreate
+  = InvitationCreate {
+    invitationCreateEmail :: String,
+    invitationCreateAuthority :: Authority
+  } deriving (Eq, Show)
+
+$(deriveJSON options { fieldLabelModifier = (\(c:cs) -> toLower c : cs) . drop 16 } ''InvitationCreate)
